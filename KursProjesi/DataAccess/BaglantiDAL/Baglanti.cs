ddf58a28c07b925fb8e32173645ce259b25fe36c
Web ve Mobil Prog <Web ve Mobil Prog@DESKTOP-8M7D7GE>
2022-03-03 15:01:45 +0300

    Add project files.

---
+++ KursProjesi/DataAccess/BaglantiDAL/Baglanti.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursProjesi.DataAccess.BaglantiDAL
+{
+    public static class Baglanti //Program boyunca değişmesin diye
+                                 //static başta yarattığımız nesnesin değişmesini istemediğimiz için kıllanırız
+    {
+        private static SqlConnection baglantiNesnesi;
+
+        public static SqlConnection BaglantiNesnesi
+        {
+            get //veriyi alırken get tetiklenir
+            {
+                if(baglantiNesnesi==null)
+                { 
+                     baglantiNesnesi = new SqlConnection(ConfigurationManager.ConnectionStrings["CsKurslar"].ConnectionString);//CsKurslar yerine 0 da yazblirsin
+                }
+                return baglantiNesnesi; 
+            }
+            set 
+            {
+                baglantiNesnesi = value; 
+            }
+        }
+
+        public static void Ac()
+        {
+            if (BaglantiNesnesi.State==ConnectionState.Closed)
+            {
+                BaglantiNesnesi.Open();
+            }
+        }
+        public static void Kapa()
+        {
+            if (BaglantiNesnesi.State!=ConnectionState.Open) BaglantiNesnesi.Close();
+            
+        } 
+
+    }
+}
---
+++ KursProjesi/Entity/Kurs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursProjesi.Entity //Veri tabanındaki kayıtları listelerekn, ınsert,update işlemlerinde kullanılır.
+                             //içinde verileri tutar.İçinde alanları bulunan sınıflar
+{
+    class Kurs
+    {
+        public int ID { get; set; }
+        public string Ad { get; set; }
+        public string Sorumlu { get; set; }
+        public DateTime BaslangicTarihi { get; set; }
+        public int Sure { get; set; }
+    }
+}
